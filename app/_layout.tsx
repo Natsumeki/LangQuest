b0eb3f83966aafc8a35962c6180bdea36c203344
Your Name <you@example.com>
2025-04-08 19:13:25 +0300

    layout and sign panel

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import '../global.css';
-
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
+    <ClerkProvider tokenCache={tokenCache}>
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="gameScreen" options={{ headerShown: true }}/>
@@ -11,5 +14,7 @@ export default function Layout() {
       <Stack.Screen name="rules" options={{ headerShown: true }}/>
       <Stack.Screen name="profileScreen" options={{ headerShown: false }} />
     </Stack>
+    </ClerkProvider>
   );
+  
 }
